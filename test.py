ef2f34ecab41e2745fcaf3ec096ac5cdf1578de1
RobertsRob <83211059+RobertsRob@users.noreply.github.com>
2024-07-04 11:12:11 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,8 @@
+def func_1(num_1, num_2):
+    for y_i in range(num_2):
+        for x_i in range(num_1):
+            # print(" * ", end="")
+            print(" "+str((x_i+y_i)%10)+" ", end="")
+        print()
+
+func_1(10,10)  # x, y
