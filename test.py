b0c850153d294d4e2c23a104536d00b7e2537f08
RobertsRob <83211059+RobertsRob@users.noreply.github.com>
2024-07-04 11:14:01 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 def func_1(num_1, num_2):
     for y_i in range(num_2):
         for x_i in range(num_1):
-            # print(" * ", end="")
             print(" "+str((x_i+y_i)%10)+" ", end="")
         print()
 
